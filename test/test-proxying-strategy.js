1b406a32f1b901fb65f1212ef697095b09cad824
Sam Day <sday@atlassian.com>
2014-07-10 18:36:17 +1000

    It's a test. And it's broken. Noes.

--- test/test-proxying-strategy.js
+++ test/test-proxying-strategy.js
@@ -3,12 +3,19 @@
 var Promise = require("bluebird"),
     hotpotato = require("../hotpotato"),
     cluster = require("cluster"),
+    Agent = require("yakaa"),
     common = require("./common");
 
 var expect = require("chai").expect;
 
 var bouncer = hotpotato("test", {
   strategies: ["proxying"]
+});
+
+var keepaliveAgent = new Agent({
+  keepAlive: true,
+  maxIdle: 1,
+  maxSockets: 1
 });
 
 describe("hotpotato proxy strategy", function() {
@@ -43,6 +50,31 @@ describe("hotpotato proxy strategy", function() {
       })
       .then(function(result) {
         expect(result).to.eql("worker" + echo.id);
+      });
+  });
+
+  it.only("bounces connections correctly", function() {
+    var self = this;
+
+    bouncer.router(function() {
+      return self.echo.id;
+    });
+
+    return common.requestToWorker(self.listenPass, { path: "/passconn" })
+      .then(function(req) {
+        req.end();
+        return common.readFully(req);
+      })
+      .then(function(text) {
+        expect(text).to.eql("worker" + self.echo.id);
+        return common.requestToWorker(self.listenPass, { path: "/foo"});
+      })
+      .then(function(req) {
+        req.end();
+        return common.readFully(req);
+      })
+      .then(function(text) {
+        expect(text).to.eql("worker" + self.echo.id);
       });
   });
 });
